b22a62dfab146ba5ab8bf0cfbc4c07f92a3e4c33
Takenobu <KTakenobu@mac.com>
2023-09-03 19:46:56 +0900

    first commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# How to use
+0. Prepare your Python Environment  
+If you don't have Python Enviroment, you need setup your Enviroment.  
+Recommended : (https://docs.conda.io/projects/miniconda/en/latest/miniconda-install.html)
+
+1. import library  
+Run below in command line  
+    `pip install pysimplegui`
+
+3. Run programm  
+Run program  
+    `python topick_select.py`
+
+2. input your text in window  
+You can put your text in the first text box.
+
+3. You wiil get the one of line form text  
+Code output a line from your input text in the second text box.
+
+# Reference
+1. https://qiita.com/KatsunoriNakamura/items/376da645e52f7ef7f9ef
---
+++ topick_select.py
@@ -0,0 +1,39 @@
+# 参考
+# 1. https://qiita.com/KatsunoriNakamura/items/376da645e52f7ef7f9ef
+
+import PySimpleGUI as sg
+import random
+
+
+# テーマ決め
+sg.theme('DarkAmber') 
+
+# All the stuff inside your window.
+layout = [  [sg.Text('Enter your text')],
+            [sg.Multiline(size=(50,10),key='input')],
+            [sg.Button('Ok'), sg.Button('Cancel')],
+            [sg.Text('picked up Topics!!!')],
+            [sg.Multiline(size=(50,10),key='output')]
+            ]
+
+
+# Create the Window
+window = sg.Window('TOPIC selecter!', layout, size=(800,800))
+# Event Loop to process "events" and get the "values" of the inputs
+while True:
+    # read contents
+    event, values = window.read()
+    # Cancel all
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+
+    # read contents
+    multiline_text = values['input']
+    input_lines = multiline_text.split()
+    # pick up one topic
+    output_line = input_lines[random.randint(0,len(input_lines)-1)]
+
+    if event == 'Ok':
+        window['output'].Update(output_line)
+
+window.close()
